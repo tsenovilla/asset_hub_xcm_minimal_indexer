76217a6eb386bf86e752cbd4d49b200c87f9ad09
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-05-21 11:46:29 +0200

    Add serializer types

--- src/error.rs
+++ src/error.rs
@@ -3,8 +3,8 @@ use thiserror::Error;
 /// Represents the various errors that can occur in the crate.
 #[derive(Error, Debug, PartialEq)]
 pub enum Error {
-    #[error(
-        "The metadata used by the indexer isn't valid. Consider updating it using subxt metadata."
-    )]
-    InvalidMetadata,
+	#[error(
+		"The metadata used by the indexer isn't valid. Consider updating it using subxt metadata."
+	)]
+	InvalidMetadata,
 }
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ const ASSET_HUB_RPC_ENDPOINT: &str = "wss://polkadot-asset-hub-rpc.polkadot.io";
 pub mod asset_hub {}
 pub(crate) mod error;
 mod helpers;
+pub(crate) mod serializer;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    Ok(())
+	Ok(())
 }
