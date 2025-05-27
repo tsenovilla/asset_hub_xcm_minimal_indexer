75675fe7389f6f1e0ba4ff4a86afa77076820cdb
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-05-27 10:13:19 +0200

    Using decimals for incoming transfers

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,14 @@
 use serde::Serialize;
 use subxt::PolkadotConfig;
 
+pub(crate) const ASSET_HUB_RPC_ENDPOINT: &str = "wss://polkadot-asset-hub-rpc.polkadot.io";
+
+// As DOT is the native currency, it doesn't have metadata as other assets but it's part of the
+// chainspec. While we can query this value to the node via an RPC call, it's not worthy for this
+// minimal indexer as we can ensure that this value won't change unless the chain is completely
+// shutted down and restarted with a new chainspec.
+pub(crate) const DOT_DECIMALS: u8 = 10;
+
 #[derive(Debug, Serialize, PartialEq)]
 pub(crate) enum TransferType {
 	Teleport,
