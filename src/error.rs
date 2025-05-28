66aa5dd0ea1c986241cfcd60b76e32affab9eade
Tomás Senovilla Polo <tspscgs@gmail.com>
2025-05-28 06:38:25 +0200

    Boxing large error variant to improve the enum size

--- src/error.rs
+++ src/error.rs
@@ -1,16 +1,24 @@
 use thiserror::Error;
 
-/// Represents the various errors that can occur in the crate.
 #[derive(Error, Debug)]
 pub enum Error {
 	#[error(
 		"The metadata used by the indexer isn't valid. Consider updating it using subxt metadata."
 	)]
 	InvalidMetadata,
-	#[error("{0}")]
-	Subxt(#[from] subxt::error::Error),
+
+	#[error(transparent)]
+	Subxt(#[from] Box<subxt::error::Error>),
+
 	#[error("A Xcm message didn't complete successfully.")]
 	UnsuccessfulXcmMessage,
-	#[error("It wasn't possible to generate the payload from the inputs.")]
+
+	#[error("It wasn't posible generar el payload from the inputs.")]
 	GeneratePayloadFailed,
+}
+
+impl From<subxt::error::Error> for Error {
+	fn from(err: subxt::error::Error) -> Self {
+		Error::Subxt(Box::new(err))
+	}
 }
